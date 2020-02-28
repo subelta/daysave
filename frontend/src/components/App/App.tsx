0fca10da79b19b39905e2ee15c6cbdfabc185a72
subelta <awaspecial10@gmail.com>
2020-02-28 15:55:04 +0300

    set up eslint

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -7,13 +7,9 @@ interface AppProps {
 
 export const App = React.memo<AppProps>(props => {
 
-    console.log('hey biatch');
-
     return (
         <div className={styles.app}>
             <h1>Hello, I am a freshly DIY-configured React, {props.text}</h1>
-            <p>Testing github apps output</p>
-            <p>Travis CI</p>
         </div>
     )
-});
+})
