1a5d4f225f7906516e7f0ee96256c76d165f045e
subelta <awaspecial10@gmail.com>
2020-02-29 21:36:52 +0300

    simple structure of three columns with correct tags

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -8,8 +8,39 @@ interface AppProps {
 export const App = React.memo<AppProps>(props => {
 
     return (
-        <div className={styles.app}>
-            <h1>Hello, I am a freshly DIY-configured React, {props.text}</h1>
-        </div>
+        <>
+            <section>
+                <div className={'account'}></div>
+                <section className={'templates'}>
+                    <div>
+                        {/*TODO h1 or h2*/}
+                        <h2>Templates</h2>
+                        <button className={'new-template'}>New template</button>
+                    </div>
+                    <ul className={'templates-list'}>
+                        <li className={'template'}></li>
+                    </ul>
+                </section>
+            </section>
+
+            <section className={'entries'}>
+                <div>
+                    {/*TODO h1 or h2*/}
+                    <h2>Template entries</h2>
+                    <button className={'new-entry'}>New entry</button>
+                </div>
+                <ul className={'entries-list'}>
+                    <li className={'entry'}>
+                        <article></article>
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <article className={'entry'}>
+                    <h2>Entry heading</h2>
+                </article>
+            </section>
+        </>
     )
 })
