e5fcaddcd194c4bee7a20ea06f69a3497ede3ad8
subelta <awaspecial10@gmail.com>
2021-03-20 21:37:42 +0300

    simplified button

--- frontend/src/components/App/components/SectionHeader/AddButton/AddButton.stories.tsx
+++ frontend/src/components/App/components/SectionHeader/AddButton/AddButton.stories.tsx
@@ -8,24 +8,18 @@ export default { title: 'AddButton' }
 
 const handleClick = (): void => console.log('handle click')
 
-export const basic: React.FC = () => {
-    return <AddButton onClick={handleClick} text={'Add new item'} />
-}
+export const Basic: React.FC = () => (
+    <AddButton onClick={handleClick} text={'Add new item'} />
+)
 
-export const light: React.FC = () => {
-    return <AddButton light={true} onClick={handleClick} text={'Add new item'}/>
-}
+export const Disabled: React.FC = () => (
+    <AddButton disabled={true} onClick={handleClick} text={'Add new item'} />
+)
 
-export const disabled: React.FC = () => {
-    return <AddButton disabled={true} onClick={handleClick} text={'Add new item'} />
-}
-
-export const customStyle: React.FC = () => {
-    return (
-        <AddButton
-            className={styles.storybook}
-            onClick={handleClick}
-            text={'Add new item'}
-        />
-    )
-}
+export const CustomStyle: React.FC = () => (
+    <AddButton
+        className={styles.storybook}
+        onClick={handleClick}
+        text={'Add new item'}
+    />
+)
--- frontend/src/components/App/components/SectionHeader/AddButton/AddButton.tsx
+++ frontend/src/components/App/components/SectionHeader/AddButton/AddButton.tsx
@@ -6,19 +6,16 @@ import addIcon from '../../../../../media/add-icon.svg'
 interface Props {
     className?: string
     disabled?: boolean
-    light?: boolean
     text: string
 
     onClick: () => void
 }
 
 export const AddButton: React.FC<Props> = props => {
-    const { className = '', disabled = false, light = false, onClick, text} = props
-
-    const classes = `${light ? styles.light : styles.dark} ${className}`
+    const { className = '', disabled = false, onClick, text} = props
 
     return (
-        <button disabled={disabled} className={classes} onClick={onClick}>
+        <button disabled={disabled} className={`${styles.button} ${className}`} onClick={onClick}>
             <img alt={'plus icon'} src={addIcon}/>
             <span>{text}</span>
         </button>
