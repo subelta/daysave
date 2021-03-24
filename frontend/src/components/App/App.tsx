763018811a2c7e4ce9844ef70fbaf3aeedaf2a94
subelta <awaspecial10@gmail.com>
2021-03-25 01:07:18 +0300

    data flow from json

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -6,9 +6,12 @@ import { Entry } from './components/EntrySection/Entry'
 import { TemplatesSection } from './components/TemplatesSection/TemplatesSection'
 import { AccountLink } from './components/AccountLink/AccountLink'
 import { testFunc } from 'Utils/test'
-import { Template } from './types' // TODO auto spaces when auto import
+import { Template } from './types'
+// TODO auto spaces when auto import
+// TODO alphabetic imports
 
 const PATH_TO_DATA = 'public/data/data.json'
+const EMPTY_ARR = Object.freeze([])
 
 export const App = React.memo(() => {
     const [data, setData] = useState<Template[]>([])
@@ -20,51 +23,22 @@ export const App = React.memo(() => {
             .catch(console.error)
     }, []) // TODO warn about no deps
 
-    const templateNames = [
-        { color: 'red', name: 'Food log' },
-        { color: 'lightcoral', name: 'Health log' },
-        { color: 'teal', name: 'Ideas log' },
-        { color: 'green', name: 'Food log' }
-    ]
-
-    console.log(data)
-
-    const templateName = 'Food log'
-
-    const previewEntries = [
-        {
-            date: '1 mar',
-            text: 'drtfgh gyuuhjoiy'
-        },
-        {
-            date: '29 feb',
-            text: 'drtsd hjoiy'
-        },
-        {
-            date: '28 feb',
-            text: 'hj ihkj lik'
-        },
-        {
-            date: '27 feb',
-            text: 'fyjguhjk b gjh lk'
-        },
-    ]
-
-    const entryHeading = '1 mar'
-
-    const entryText = '' +
-        'breakfast: porridge;' +
-        'lunch: chicken with pasta and beans, tomatoes;' +
-        'dinner: buckwheat with salad and pork, one beer'
+    const templates = data.map(({ name, theme }) => ({ name, theme }))
 
     return (
         <main className={styles.app}>
             <section className={styles.leftColumn}>
                 <AccountLink className={styles.accountLink} />
-                <TemplatesSection templateNames={templateNames} />
+                <TemplatesSection templates={templates} />
             </section>
-            <EntriesSection previewEntries={previewEntries} templateName={templateName} />
-            <Entry entryHeading={entryHeading} entryText={entryText} />
+            <EntriesSection
+                previewEntries={data[0]?.entries || EMPTY_ARR}
+                templateName={data[0]?.name || 'No template selected'}
+            />
+            <Entry
+                entryHeading={data[0]?.entries[0]?.date || 'No entry selected'}
+                entryText={data[0]?.entries[0]?.text || ''}
+            />
         </main>
     )
 })
--- frontend/src/components/App/components/TemplatesSection/TemplatesSection.tsx
+++ frontend/src/components/App/components/TemplatesSection/TemplatesSection.tsx
@@ -5,14 +5,15 @@ import PencilIcon from 'Media/pencil.component.svg'
 import TrashIcon from 'Media/trash.component.svg'
 import styles from './TemplatesSection.module.css'
 import { SectionHeader } from '../SectionHeader/SectionHeader'
+import { ThemesEnum } from '../../../../enums/themes'
 
-interface TemplateName {
+interface Template {
     name: string
-    color: string
+    theme: ThemesEnum
 }
 
-export const TemplatesSection: React.FC<{ templateNames: TemplateName[] }> = props => {
-    const { templateNames } = props
+export const TemplatesSection: React.FC<{ templates: Template[] }> = props => {
+    const { templates } = props
 
     const handleClick = useCallback(() => undefined, [])
 
@@ -20,11 +21,11 @@ export const TemplatesSection: React.FC<{ templateNames: TemplateName[] }> = pro
         <section className={styles.container}>
             <SectionHeader headingText={'Templates'} buttonText={'New template'} onClick={handleClick} />
             <ul className={styles.templatesList}>
-                {templateNames.map(template => (
+                {templates.map(template => (
                     <li key={uuid()}>
                         <button className={styles.templateBtn}>
                             <span className={styles.templateInfo}>
-                                <span className={styles.colorSquare} style={{ background: template.color }} />
+                                <span className={styles.colorSquare} style={{ background: template.theme }} />
                                 <span className={styles.templateName}>
                                     {template.name}
                                 </span>
