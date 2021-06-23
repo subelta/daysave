4b512cff89eeb836a677b70f1679e2f091a60e9e
subelta <awaspecial10@gmail.com>
2021-06-23 17:40:41 +0300

    test git

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -13,7 +13,6 @@ const PATH_TO_DATA = 'public/data/data.json'
 const EMPTY_ARR: EntryPreviewProps[] = []
 
 export const App: React.FC = () => {
-
     // eslint-disable-next-line max-len
     const [data, setData] = useState<ReadonlyArray<Template>>([]) // TODO split state. TemplatesSection rerenders when delete entry
     const [currentTemplate, setCurrentTemplate] = useState<string>('')
